3b161c754da71b42bd5289aec6eb1d3b68ba5f5c
Alan Zhao <alanzhaonys@gmail.com>
2021-07-24 18:15:42 -0400

    splash screen

--- src/App.js
+++ src/App.js
@@ -12,14 +12,85 @@ import { Footer } from './components/Footer';
 import './scss/app.scss';
 
 class App extends Page {
+  constructor(props) {
+    super(props);
+
+    console.log(process.env.NODE_ENV);
+
+    // Database loadion statuses
+    this.loadStatuses = {
+      LOADING: "loading",
+      FAILED: "failed",
+      LOADED: "loaded"
+    };
+
+    // Initial states
+    this.state = {
+      loadStatus: this.loadStatuses.LOADING
+    };
+  }
+
+  setStatus(status) {
+    this.setState({
+      loadStatus: status
+    });
+  }
+
+  async componentDidMount() {
+    try {
+      let loaded = true;
+      if (loaded) {
+        // Add delay for UX
+        setTimeout(() => {
+          // Update db status
+          this.setStatus(this.loadStatuses.LOADED);
+
+          // Change color <body>
+          document.body.classList.add("body-no-gradient");
+        }, 2000);
+      } else {
+        // Add delay for UX
+        setTimeout(() => {
+          // Update db status
+          this.setStatus(this.loadStatuses.FAILED);
+        }, 2000);
+      }
+    } catch (exception) {
+      // Add delay for UX
+      setTimeout(() => {
+        // Update db status
+        this.setStatus(this.loadStatuses.FAILED);
+      }, 2000);
+    }
+  }
+
+
   render() {
-    return (
-      <BrowserRouter>
-        <Header />
-        <Main />
-        <Footer />
-      </BrowserRouter>
-    );
+    const loadStatus = this.state.loadStatus;
+
+    if (loadStatus === this.loadStatuses.LOADING) {
+      return (
+        <div className={loadStatus}>
+          <i className="fas fa-spin fa-sync" />
+        </div>
+      );
+    } else if (loadStatus === this.loadStatuses.FAILED) {
+      return (
+        <div className={loadStatus}>
+          <i className="fas fa-exclamation-triangle" /> Unable to load,
+          <br />
+          close the application and try again.
+        </div>
+      );
+    } else if (loadStatus === this.loadStatuses.LOADED) {
+      return (
+        <BrowserRouter>
+          <Header />
+          <Main />
+          <Footer />
+        </BrowserRouter>
+      );
+    }
   }
 }
 
