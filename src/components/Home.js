16e7af5f523ea68004d1bfd711c420e56cdc1e09
Alan Zhao <alanzhaonys@gmail.com>
2021-07-24 13:04:58 -0400

    additional demo

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,32 +22,44 @@ export class Home extends Page {
       0.1,
       2000
     );
-    //camera.rotation.y = (45 / 180) * Math.PI;
-    camera.position.set(10, 0, 5);
+    camera.position.set(0, 0, 50);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     //let hlight = new THREE.AmbientLight(0x404040, 100);
     //scene.add(hlight);
 
-    /*let directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+    let directionalLight = new THREE.DirectionalLight(0xffffff, 10);
     directionalLight.position.set(0, 1, 0);
     directionalLight.castShadow = true;
     scene.add(directionalLight);
 
-    let light = new THREE.PointLight(0xc4c4c4, 10);
+    let light = new THREE.PointLight(0xc4c4c4, 1);
     light.position.set(0, 300, 500);
     scene.add(light);
 
-    let light2 = new THREE.PointLight(0xc4c4c4, 10);
+    let light2 = new THREE.PointLight(0xc4c4c4, 1);
     light2.position.set(500, 100, 0);
-    scene.add(light2);*/
+    scene.add(light2);
 
-    let light3 = new THREE.PointLight(0xc4c4c4, 10);
+    let light3 = new THREE.PointLight(0xc4c4c4, 1);
     light3.position.set(0, 100, -500);
     scene.add(light3);
 
-    let light4 = new THREE.PointLight(0xc4c4c4, 10);
+    let light4 = new THREE.PointLight(0xc4c4c4, 1);
     light4.position.set(-500, 300, 500);
     scene.add(light4);
+
+    /*
+    const dirLight = new THREE.DirectionalLight(0xffffff, 2);
+    dirLight.position.set(0, 0, 1).normalize();
+    scene.add(dirLight);
+    */
+
+    /*
+    const pointLight = new THREE.PointLight(0xffffff, 3);
+    pointLight.position.set(0, 100, 90);
+    scene.add(pointLight);
+    */
 
     let renderer = new THREE.WebGLRenderer({ antialias: true });
     //renderer.setSize(window.innerWidth, window.innerHeight);
@@ -62,21 +74,68 @@ export class Home extends Page {
 
     let loader = new GLTFLoader();
     let mesh = null;
-    loader.load("shiba/scene.gltf", gltf => {
+    loader.load("skull/scene.gltf", gltf => {
       mesh = gltf.scene;
-      mesh.scale.set(5, 5, 5);
+      mesh.scale.set(15, 15, 15);
       mesh.position.set(0, 0, 0);
       //pivot.add( mesh );
 
-      const box = new THREE.Box3().setFromObject(mesh);
-      const boxHelper = new THREE.Box3Helper(box, 0xffff00);
-      scene.add(boxHelper);
-      let center = box.getCenter(new THREE.Vector3());
-      let size = box.getSize(new THREE.Vector3());
-      mesh.position.set(-center.x, size.y / 2 - center.y, -center.z);
+      //const box = new THREE.Box3().setFromObject(mesh);
+      //const boxHelper = new THREE.Box3Helper(box, 0xffff00);
+      //scene.add(boxHelper);
+
+      //let center = box.getCenter(new THREE.Vector3());
+      //let size = box.getSize(new THREE.Vector3());
+      //mesh.position.set(-center.x, size.y / 2 - center.y, -center.z);
+      mesh.position.set(0, 0, 0);
 
       scene.add(mesh);
       animate();
+    });
+
+    // add 3D text beveled and sized
+    const fontLoader = new THREE.FontLoader();
+    fontLoader.load("fonts/nobile_regular.typeface.json", function(font) {
+      let textGeo = new THREE.TextGeometry("ARE YOU LOST?", {
+        font: font,
+        size: 13,
+        height: 1,
+        curveSegments: 5,
+        bevelThickness: 1,
+        bevelSize: 1,
+        bevelEnabled: true
+      });
+
+      let material = new THREE.MeshPhongMaterial({
+        color: "red"
+      });
+
+      let textMesh = new THREE.Mesh(textGeo, material);
+      //textMesh.scale.set(0.5, 0.5, 0.5);
+
+      textGeo.computeBoundingBox();
+      const centerX =
+        -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
+      // Center Y
+      //const centerY = -0.5 * ( textGeo.boundingBox.max.y - textGeo.boundingBox.min.y );
+      const centerY =
+        -2.5 * (textGeo.boundingBox.max.y - textGeo.boundingBox.min.y);
+
+      //let center = box.getCenter(new THREE.Vector3());
+      //let size = box.getSize(new THREE.Vector3());
+      //textMesh.position.set(-center.x, size.y / 2 - center.y, -center.z);
+
+      textMesh.position.set(centerX, centerY, 0);
+
+      textMesh.rotation.x = 0;
+      textMesh.rotation.y = 0;
+      textMesh.rotation.z = 0;
+
+      //const box = new THREE.Box3().setFromObject(textMesh);
+      //const boxHelper = new THREE.Box3Helper(box, 0xffff00);
+      //scene.add(boxHelper);
+
+      scene.add(textMesh);
     });
 
     refresh();
@@ -98,6 +157,11 @@ export class Home extends Page {
     return (
       <div className="page">
         <div id="scene"></div>
+        <ul id="menu">
+          <li>Menu 1</li>
+          <li>Menu 2</li>
+          <li>Menu 3</li>
+        </ul>
       </div>
     );
   }
